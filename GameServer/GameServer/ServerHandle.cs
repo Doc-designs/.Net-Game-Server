f1a110f759b5769bacd5d91d1acc0c0fbc4a025e
Doc-designs <70421267+Doc-designs@users.noreply.github.com>
2021-01-20 03:19:10 -0800

    Player Movement Cpaacity

--- GameServer/GameServer/ServerHandle.cs
+++ GameServer/GameServer/ServerHandle.cs
@@ -32,11 +32,5 @@ namespace GameServer
 
             Server.clients[_fromClient].player.SetInput(_inputs, _rotation);
         }
-        public static void UDPTestReceived(int _fromClient, Packet _packet)
-        {
-            string _msg = _packet.ReadString();
-
-            Console.WriteLine($"Received packet via UDP. Contains message: {_msg}");
-        }
     }
 }
